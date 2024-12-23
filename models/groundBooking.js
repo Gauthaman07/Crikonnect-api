d5f1445d27fdd867bc3ce6a25bb55c6e4205f227
Gautham <gauthaman101@gmail.com>
2024-12-23 15:50:46 +0530

    morning

--- models/groundBooking.js
+++ models/groundBooking.js
@@ -5,7 +5,7 @@ const groundBookingSchema = new Schema({
     groundId: { type: mongoose.Schema.Types.ObjectId, ref: 'Ground', required: true },
     bookedByTeam: { type: mongoose.Schema.Types.ObjectId, ref: 'Team', required: true },
     bookedDate: { type: Date, required: true },
-    timeSlot: { type: String, enum: ['Morning', 'Afternoon'], required: true },
+    timeSlot: { type: String, enum: ['morning', 'afternoon'], required: true },
     status: { type: String, enum: ['pending', 'booked', 'rejected'], default: 'pending' },
     createdAt: { type: Date, default: Date.now },
 });
