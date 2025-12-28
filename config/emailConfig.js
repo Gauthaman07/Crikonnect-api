76373c981bc5ef417ec02e31a91df1e3d944098e
Gauthaman07 <119770520+Gauthaman07@users.noreply.github.com>
2025-12-28 19:09:25 +0530

    smtp

--- config/emailConfig.js
+++ config/emailConfig.js
@@ -2,10 +2,15 @@ require('dotenv').config();
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false, // Use STARTTLS
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASSWORD
+    },
+    tls: {
+        rejectUnauthorized: false
     }
 });
 
